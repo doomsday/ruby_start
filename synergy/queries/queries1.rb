bde0ad2172586ab61e26b6930b1b65a2dff7aa6d
Mikhail Gashenko <dr.psycho@live.com>
2017-08-20 02:24:37 +0600

    20_08_2017

--- synergy/queries/queries1.rb
+++ synergy/queries/queries1.rb
@@ -1,8 +1,21 @@
-@query1 = { data: { :$elemMatch => { value: Regexp.new('^ТОО') } } }
+
+# Partial match queries
+@query1_0 = { 'data.value' => /^ТОО/ }
+@query1_1 = { 'data.value' => Regexp.new('^ТОО') }
+@query1_2 = { 'data.value' => { :$regex => '^ТОО', :$options => 'i' } }
+
+# Literal match
 @query2 = { uuid: '618a22b9-a83c-4e61-b5fb-4da814e86499' }
-@query3 = { :$and =>
-              [
-                { data: { :$elemMatch => { id: 'sum_supply' } } },
-                { data: { :$elemMatch => { key: { :$gt => 5_000_000 } } } }
-              ] }
-@query4 = { data: {:$elemMatch => {id: 'sum_supply', key: {:$gt => 5_000_000}}}}
+
+# Querying specific ranges
+@query3 = {
+  data: { :$elemMatch => {
+    id: 'sum_supply',
+    key: { :$gte => 5_000_000, :$lte => 8_000_000 }
+  } }
+}
+
+@query4 = { 'data.id' => 'sum_supply', 'data.key' => {:$gte => 5_000_000, :$lte => 8_000_000} }
+
+# JS where
+@query5 = { :$where => 'function() {return this.data.id = "sum_supply"}' }
--- synergy/queries/query_executor.rb
+++ synergy/queries/query_executor.rb
@@ -6,7 +6,7 @@ require './queries1'
 
 @payments_data = mongo_connect('synergy', 'payments_asf_data')
 
-@result = @payments_data.find(@query4).limit(50).projection('data.$' => 1)
+@result = @payments_data.find(@query1_2).limit(50).projection('data.$' => 1)
 
 unless @result.to_a.empty?
   @result.each do |row|
